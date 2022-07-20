efdd157785cc6ace0a8463f7a898ad64e809257a
Cahill-99 <ecahill001@gmail.com>
2022-07-20 16:30:16 -0230

    nav-menu started

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -6,6 +6,25 @@ function Navmenu(){
     return(
         <div className='navmenu-background'>
 
+            <div className='welcome-block'>
+
+            </div>
+            <div className='task-button'>
+                <img src= {process.env.PUBLIC_URL +'/Icons/check-mark-svgrepo-com'}></img>
+                <h4>Tasks</h4>
+            </div>
+            <div className='inbox-button'>
+                <h4>Inbox</h4>
+            </div>
+            <div className='notif-button'>
+                <h4>Notifications</h4>
+            </div>
+            <div className='marks-button'>
+                <h4>Marks</h4>
+            </div>
+            <div className='settings-button'>
+                <h4>Settings</h4>
+            </div>
         </div>
     )
 }
