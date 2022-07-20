066b28e56a2da7296bda06530b78b552f17ff3ea
Cahill-99 <ecahill001@gmail.com>
2022-07-20 15:41:39 -0230

    added date placeholder

---
+++ src/components/Statusgroup.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Statusgroup(){
+
+
+    return(
+        <div className='status-group'>
+            <div className='date-block'>
+                <h3>Apr</h3>
+                <h1>28</h1>
+            </div>
+            <div className='status-right-wrapper'>
+                <div className='progress-wrapper'>
+
+                </div>
+                <div className='notices-wrapper'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Statusgroup;
