9345939b2f388a4d57d2c688d187ba078f6845f3
Cahill-99 <ecahill001@gmail.com>
2022-07-23 13:27:19 -0230

    added all navbar icons

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -10,19 +10,23 @@ function Navmenu(){
 
             </div>
             <div className='task-button'>
-                <img src= {process.env.PUBLIC_URL +'/Icons/check-mark-svgrepo-com'}></img>
+                <img src= "/Icons/check-mark-svgrepo-com.svg" className = "navicon"></img>
                 <h4>Tasks</h4>
             </div>
             <div className='inbox-button'>
+                <img src= "/Icons/mail-svgrepo-com.svg" className = "navicon"></img>
                 <h4>Inbox</h4>
             </div>
             <div className='notif-button'>
+                <img src= "/Icons/bell-svgrepo-com.svg" className = "navicon"></img>
                 <h4>Notifications</h4>
             </div>
             <div className='marks-button'>
+                <img src= "/Icons/diploma-svgrepo-com.svg" className = "navicon"></img>
                 <h4>Marks</h4>
             </div>
             <div className='settings-button'>
+                <img src= "/Icons/gear-svgrepo-com.svg" className = "navicon"></img>
                 <h4>Settings</h4>
             </div>
         </div>
